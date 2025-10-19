4e7cbee23bebe865b744be889a690f486aa140df
ShahmeerAli <shahmeer.devel@gmail.com>
2025-10-19 14:18:05 +0500

    added splash Screen

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:travelmate/main.dart';
 
 class SplashScreen extends StatefulWidget{
   @override
@@ -9,10 +13,24 @@ class SplashScreen extends StatefulWidget{
 }
 
 class SplashState extends State<SplashScreen>{
+
+
+ @override
+  void initState() {
+
+    super.initState();
+    Timer(Duration(seconds: 4), (){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>MyApp()));
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Center(
+         child:Lottie.asset("assets/raw/splash.json") ,
+      ),
     );
   }
 
